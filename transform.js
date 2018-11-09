7466168d525e704c230a605f4ead08cb59612dc5
Justin Raphael Donnaruma <justin@donnaruma.us>
2018-11-09 09:30:43 -0500

    Added console output for process information

--- transform.js
+++ transform.js
@@ -1,17 +1,20 @@
 const fs = require('fs')
 
+console.log('###### Loading file...')
 const rawData = fs.readFileSync('data.json')
+
+console.log('###### Parsing data...')
 const data = JSON.parse(rawData)
 
-console.log (data)
-
+console.log('###### Mapping data...')
 const transformedData = data.map(obj =>{
     obj.customerId = obj.customer.id
     delete(obj.customer)
     return obj
 });
-console.log(transformedData)
 
+console.log('###### Writting data to file...')
 const rawTransformedData = JSON.stringify(transformedData)
-
 fs.writeFileSync('data-transformed.json', rawTransformedData, 'utf8')
+
+console.log('###### Done.')
