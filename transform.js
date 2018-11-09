3aceb740cce4874e67162dab6b0f50b807e10171
Justin Raphael Donnaruma <justin@donnaruma.us>
2018-11-09 09:13:14 -0500

    Finalized reading / writing to files.

--- transform.js
+++ transform.js
@@ -1,9 +1,17 @@
 const fs = require('fs')
 
+const rawData = fs.readFileSync('data.json')
+const data = JSON.parse(rawData)
+
+console.log (data)
+
 const transformedData = data.map(obj =>{
     obj.customerId = obj.customer.id
     delete(obj.customer)
     return obj
 });
+console.log(transformedData)
 
-console.log(transformedData);
+const rawTransformedData = JSON.stringify(transformedData)
+
+fs.writeFileSync('data-transformed.json', rawTransformedData, 'utf8')
